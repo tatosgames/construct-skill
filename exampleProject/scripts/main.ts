836c5357ffad92e9118583d5e8b377401307e6d3
Luca Contato <tatosgames@users.noreply.github.com>
2026-06-09 12:38:28 +0100

    Update main.ts

--- exampleProject/scripts/main.ts
+++ exampleProject/scripts/main.ts
@@ -2,11 +2,16 @@
 // Import any other script files here, e.g.:
 // import * as myModule from "./mymodule.js";
 
+let keyboard: IKeyboardObjectType;
+let playerInst: IWorldInstance;
+
+const PLAYER_SPEED = 200; // pixels per second
+
 runOnStartup(async runtime =>
 {
 	// Code to run on the loading screen.
 	// Note layouts, objects etc. are not yet available.
-	
+
 	runtime.addEventListener("beforeprojectstart", () => OnBeforeProjectStart(runtime));
 });
 
@@ -15,11 +20,23 @@ async function OnBeforeProjectStart(runtime: IRuntime)
 	// Code to run just before 'On start of layout' on
 	// the first layout. Loading has finished and initial
 	// instances are created and available to use here.
-	
+
+	keyboard = runtime.keyboard;
+	playerInst = runtime.objects.player.getFirstInstance()!;
+
 	runtime.addEventListener("tick", () => Tick(runtime));
 }
 
 function Tick(runtime: IRuntime)
 {
-	// Code to run every tick
+	const speed = PLAYER_SPEED * runtime.dt;
+
+	if (keyboard.isKeyDown("ArrowLeft"))
+		playerInst.x -= speed;
+	if (keyboard.isKeyDown("ArrowRight"))
+		playerInst.x += speed;
+	if (keyboard.isKeyDown("ArrowUp"))
+		playerInst.y -= speed;
+	if (keyboard.isKeyDown("ArrowDown"))
+		playerInst.y += speed;
 }
